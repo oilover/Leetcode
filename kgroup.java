cc5930ec7adc6e98947976763c74a4f8aec673f0
longago <1310421753@qq.com>
2018-04-11 08:04:12 +0800

    Create kgroup.java

---
+++ kgroup.java
@@ -0,0 +1,13 @@
+ListNode tmp = head, ret = null;
+        int c = 1;
+        while (tmp!=null && tmp.next!=null) {
+        	ListNode p = rever(tmp,k);
+        	test(p);
+        	if (c==1) {
+        		ret = p;
+        	}
+        	c++;
+        	System.out.println("nextFirst:"+nextFirst);
+        	tmp.next = nextFirst;
+        	tmp = nextFirst;
+        }
