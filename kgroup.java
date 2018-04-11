11da22008656cb2a0cb55b5353425f691e0fcb8a
longago <1310421753@qq.com>
2018-04-11 08:08:29 +0800

    Update kgroup.java

--- kgroup.java
+++ kgroup.java
@@ -1,25 +1,42 @@
-ListNode tmp = head, ret = null;
-        int c = 1;
-        while (tmp!=null && tmp.next!=null) {
-        	ListNode p = rever(tmp,k);
-        	test(p);
-        	if (c==1) {
-        		ret = p;
-        	}
-        	c++;
-        	System.out.println("nextFirst:"+nextFirst);
-        	tmp.next = nextFirst;
-        	tmp = nextFirst;
+public class Solution {
+	static ListNode last,nextFirst;
+    public static ListNode rever(ListNode head, int n) {
+		if (head==null || n==0) return null;
+		if (head.next==null || n==1) {
+		//	nextFirst = head.next;
+			return head;
+		}
+		ListNode t = rever(head.next,n-1);
+		head.next.next = head;
+		head.next = null;
+		return t;
+	}
+	public static ListNode getK(ListNode p, int k) {
+		int i=0;
+		while (p!=null && p.next!=null && i<k) {
+			p=p.next; i++;
+		}
+		return p;
+	}
+	public static ListNode _reverseKGroup(ListNode head, int k) {
+		if (head==null ) return null;
+		if (head.next==null || k==1) return head;
+        ListNode tmp = head, ret = null, last = head;
+        int i;
+        for (i=0;i<k-1;i++) {
+        	if (last.next==null) break;
+        	else last = last.next;
         }
-  int c = 1;
-//        while (tmp!=null && tmp.next!=null) {
-//        	ListNode p = rever(tmp,k);
-//        	test(p);
-//        	if (c==1) {
-//        		ret = p;
-//        	}
-//        	c++;
-//        	System.out.println("nextFirst:"+nextFirst);
-//        	tmp.next = nextFirst; //rev(nextFist)
-//        	tmp = nextFirst;
-//        }
+        if (i<k-1) return head;
+        ListNode nextHead = _reverseKGroup(last.next, k);
+        ListNode result = rever(head, k);
+        head.next = nextHead;
+        return result;
+    }
+	public ListNode reverseKGroup(ListNode head, int k) {
+		if (head==null ) return null;
+		if (head.next==null || k==1) return head;
+        
+        return _reverseKGroup(head, k);
+    }
+}
