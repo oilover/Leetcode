3bb6e8aa777366d6761cb83f87d192ba01426a56
longago <1310421753@qq.com>
2018-04-11 08:08:05 +0800

    Update kgroup.java

--- kgroup.java
+++ kgroup.java
@@ -11,3 +11,15 @@ ListNode tmp = head, ret = null;
         	tmp.next = nextFirst;
         	tmp = nextFirst;
         }
+  int c = 1;
+//        while (tmp!=null && tmp.next!=null) {
+//        	ListNode p = rever(tmp,k);
+//        	test(p);
+//        	if (c==1) {
+//        		ret = p;
+//        	}
+//        	c++;
+//        	System.out.println("nextFirst:"+nextFirst);
+//        	tmp.next = nextFirst; //rev(nextFist)
+//        	tmp = nextFirst;
+//        }
