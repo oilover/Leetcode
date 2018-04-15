f9b2ffa38f2de8b3a91fb31bb2b7d1e8d3ff374f
longago <1310421753@qq.com>
2018-04-15 20:09:25 +0800

    Update Search in Rotated Array I II.cpp

--- Search in Rotated Array I II.cpp
+++ Search in Rotated Array I II.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {  // I. 
     int n;
 public:
     int search(vector<int>& a, int target) {
@@ -28,7 +28,7 @@ public:
     }
 };
 
-class Solution {
+class Solution {  // II. duplicate
     int n;
 public:
     int search(vector<int>& a, int target) {
@@ -59,5 +59,30 @@ public:
             
         }
         return false;
+    }
+};
+#define prt(k) cout<<#k" = "<<k<<endl
+class Solution {  // 153. Find Minimum in Rotated Sorted Array
+public:
+    int findMin(vector<int>& a) {
+        int n = a.size();
+        if (n==1) return a[0];
+        int l = 0, r = n-1;
+        while (l<r) {
+            int mid = (l+r) / 2;
+            // prt(mid);
+            if (a[l]<=a[r]) {
+                return a[l];
+            }
+         //  if (mid>0 && a[mid-1]<a[mid])
+            if (a[r]< a[l] && a[l]<= a[mid]) {
+                l = mid+1;
+            } else if (a[r]<a[l] && a[mid]<=a[r]){
+                r = mid;
+                // prt(r);
+            } 
+            
+        }
+        return a[l];
     }
 };
