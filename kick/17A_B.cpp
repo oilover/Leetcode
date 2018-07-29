894fa69bef7d74f5335a7a508c5b9641f5e0bc43
Liu Huang <t-lihuan@microsoft.com>
2018-07-29 12:41:25 +0800

    12

--- kick/17A_B.cpp
+++ kick/17A_B.cpp
@@ -9,6 +9,18 @@ const int MAXN = 289;
 int R,C, H[MAXN][MAXN];
 int dx[] = {0,0,1,-1};
 int dy[] = {1,-1,0,0};
+bool vis[MAXN][MAXN];
+struct P
+{
+    int H, x, y;
+    bool operator< (P p) const
+    {
+        if(H-p.H) return H < p.H;
+        if (x-p.x) return x<p.x;
+        return y<p.y;
+    }
+};
+set<P> S,T;
 bool OK(int x, int y)
 {
     for (int i=0;i<4;i++) {
@@ -17,14 +29,33 @@ bool OK(int x, int y)
     }
     return true;
 }
-struct P
+void Gao(int x, int y)
 {
-    int H, x, y;
-    bool operator< (P p) const
-    {
-        return H < p.H;
+    vis[x][y] = true;
+    P p = (P){H[x][y], x, y};
+    S.insert(p);
+    T.erase(p);
+    for (int i=0;i<4;i++) {
+        int xx = x+dx[i], yy = y+dy[i];
+        P p = (P){H[xx][yy], xx, yy};
+        if (S.find(p)!=S.end()) continue;     //printf("gao: (%d,%d)", p.x,p.y);
+        T.insert(p);
     }
-};
+}
+void print(set<P> S)
+{
+    for (P p: S) {
+        printf(" (%d,%d)", p.x,p.y);
+    } cout<<endl;
+}
+int GetMin(set<P> S)
+{
+    return (*S.begin()).H;
+}
+int GetMax(set<P> S)
+{
+    return (*S.rbegin()).H;
+}
 int main()
 {
     int _,ca=1; cin>>_;
@@ -37,20 +68,25 @@ int main()
         for (int i=0;i<R;i++) for (int j=0;j<C;j++) {
             H[i][j] -= all_min;
         }
-        set<P> S,T;
-        int lowest_edge = INF;
-        for (int i=1;i<R-1;i++) {
-            lowest_edge = min(lowest_edge, H[i][0]);
-            lowest_edge = min(lowest_edge, H[i][C-1]);
-        }
 
-        for (int j=1;j<C-1;j++) {
-            lowest_edge = min(lowest_edge, H[0][j]);
-            lowest_edge = min(lowest_edge, H[R-1][j]);
-        }
         int ans = 0;
-        for (int i=1;i<R-1;i++) for (int j=1;j<C-1;j++) {
-            ans += max(0, lowest_edge - H[i][j]);
+        memset(vis, false, sizeof vis);
+        for (int i=1;i<R-1;i++) for (int j=1;j<C-1;j++) if (OK(i,j) && !vis[i][j]){
+            S.clear(); T.clear();
+          //  S.insert((P){H[i][j], i,j});
+            Gao(i,j);
+            int now = (*T.begin()).H - (*S.rbegin()).H; int steps=0;
+            while (!T.empty() && steps++<R*C) {
+                P p = *T.begin();
+                int T_min = p.H;   //  print(S); print(T);
+                if (p.x==0||p.x==R-1 || p.y==0||p.y==C-1) {
+                    break;
+                }
+                Gao(p.x,p.y);
+                if (GetMax(S) > GetMin(T)) break;
+                now += (GetMin(T) - T_min) * S.size();
+            }
+            ans += now;
         }
         cout << "Case #" << ca++ << ": " << ans << endl;
     }
