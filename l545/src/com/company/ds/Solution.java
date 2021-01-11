5098afcfc0d2f34066756597ffec19f9f8f4497d
longago <1310421753@qq.com>
2021-01-11 11:19:00 +0800

    1

--- l545/src/com/company/ds/Solution.java
+++ l545/src/com/company/ds/Solution.java
@@ -5,6 +5,7 @@ class Solution {
         int n = key.length(), m = ring.length();
         int dp[][] = new int [n+1][m];
         int ans =  Integer.MAX_VALUE;
+        final int INF = (int)1e9;
         for (int j=1;j<m;j++) dp[0][j] = (int)1e9;
         for (int i=1;i<=n;i++) {
             for (int j=0;j<m;j++) {
@@ -13,26 +14,25 @@ class Solution {
                 if (target!=cur) {
                     continue;
                 }
-                if (target==cur) {
+                if (dp[i-1][j]<INF) {
                     dp[i][j] = dp[i-1][j]+1;
                     continue;
                 }
                 int k=(j+1)%m;
                 int t = 1;
-                for (; k!=j&&target!=ring.charAt(k); k=(k+1)%m,t++) ;
+                for (; k!=j&&dp[i-1][k]>=1e9; k=(k+1)%m,t++) ;
                 dp[i][j] = dp[i-1][k] + t + 1;
-                for (t=1, k=(j+m-1)%m; k!=j&&target!=ring.charAt(k); k=(k+m-1)%m,t++);
+                for (t=1, k=(j+m-1)%m; k!=j&&dp[i-1][k]>=INF; k=(k+m-1)%m,t++);
                 dp[i][j] = Math.min(dp[i-1][k]+t+1, dp[i][j]);
-                if (i==n) {
-                    ans = Math.min(ans, dp[i][j]);
-                }
             }
         }
+        for (int j=0;j<m;j++) ans = Math.min(ans, dp[n][j]);
         return ans;
     }
 
     public static void main(String[] args) {
         Solution s =  new Solution();
+        System.out.println(s.findRotateSteps("aaa", "aaa"));
         System.out.println(s.findRotateSteps("godding", "gd"));
     }
 }
---
+++ lc/src/main/java/tree/Solution.java
@@ -0,0 +1,95 @@
+package tree;
+/**
+ Definition for a binary tree node.
+ */
+class Node {
+    int val;
+    Node left;
+    Node right;
+    Node() {}
+    Node(int val) { this.val = val; }
+    Node(int val, Node left, Node right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+public class Solution {
+    Node print(Node u) {
+        if (u==null) {
+            return null;
+        }
+        System.out.print(u.val+" ");
+        if (u.right==null) {
+            return u;
+        }
+        return print(u.right);
+    }
+    Node rprint(Node u) {
+        if (u==null) {
+            return null;
+        }
+        System.out.print(u.val+" ");
+        if (u.left==null) {
+            return u;
+        }
+        return rprint(u.left);
+    }
+    private Node last,head;
+    Node dfs(Node u) {
+        if (u==null) return null;
+        Node pre = last;
+        last = u;
+        Node l = dfs(u.left);
+        if (l!=null) l.left = u;
+        Node t = last;
+        Node r = dfs(u.right);
+        u.left = pre; //last; 这样是循环列表
+        u.right = l==null ? r : l;
+        if (r!=null) r.left = t;
+        t.right = r;
+        return u;
+    }
+    void Dfs(Node u) { // 中序遍历转链表
+        if (u==null) return ;
+        Dfs(u.left);
+        if (last!=null) {
+            last.right = u;
+            u.left = last;
+        }else{
+            head = u;
+        }
+        last = u;
+        Dfs(u.right);
+
+        return ;
+    }
+    Node toList(Node u) {
+        last = head = null;
+        return dfs(u);
+    }
+    Node midToList(Node u) {
+        last = head = null;
+        Dfs(u);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node u = new Node(1);
+//        u.left = new TreeNode(2);
+//        u.left.left = new TreeNode(3);
+//        u.left.right = new TreeNode(4);
+//        u.right = new TreeNode(5);
+//        u.right.right = new TreeNode(6);
+//        u.right.right.left = new TreeNode(8);
+
+        u.left = new Node(2);
+//        u.right = new TreeNode(2);
+        Solution s = new Solution();
+        u = s.midToList(u);
+        Node t = s.print(u);
+        System.out.println();
+        s.rprint(t);
+    }
+}
