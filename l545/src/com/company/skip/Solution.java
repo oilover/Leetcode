4666642b9fca39c2141280b8fe78e8795649bb5d
longago <1310421753@qq.com>
2021-05-31 07:12:11 +0800

    2

--- l545/src/com/company/skip/Solution.java
+++ l545/src/com/company/skip/Solution.java
@@ -6,7 +6,6 @@ public class Solution {
     public int minSkips(int[] dist, int speed, int hoursBefore) {
         int n = dist.length;
         double dp[][] = new double[n+1][n+1];
-//        Arrays.fill(dp,(double)1e9);
         for (int i=0;i<=n;i++) {
             for (int j = 0; j <= n; j++) {
                 dp[i][j] = 1e9;
@@ -16,30 +15,29 @@ public class Solution {
         for (int i=1;i<=n;i++) {
             dp[0][i] = 0;
         }
+        final double EPS = 1e-8;
         for (int i=1;i<=n;i++) {
             for (int j=0;j<=i;j++) {
-                double t1 = Math.ceil(dp[i-1][j]) + Math.ceil(dist[i-1]*1.0/speed);
-                double t2 = (j>0?dp[i-1][j-1]:1e9) + dist[i-1]*1.0/speed;
-                dp[i][j] = Math.min(t1, t2);
+                if (i>j) {
+                    dp[i][j] = Math.min(Math.ceil(dp[i-1][j] + dist[i-1]*1.0/speed - EPS), dp[i][j]);
+                }
+                if (j>0) {
+                    dp[i][j] = Math.min(dp[i-1][j-1] + dist[i-1]*1.0/speed, dp[i][j]);
+                }
             }
         }
-        if (dp[n][n]>hoursBefore) {
-            return -1;
-        }
-        int l=0,r=n;
-        while (l<r) {
-            int mid=(l+r)/2;
-            if (dp[n][mid] <= hoursBefore+1e-4) {
-                r=mid;
-            } else {
-                l=mid+1;
+        for (int j = 0; j <= n; ++j) {
+            if (dp[n][j] < hoursBefore + EPS) {
+                return j;
             }
         }
-        return l;
+        return -1;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
+        System.out.println(s.minSkips(new int[]{1,2,3,4,5},         1000000,
+        1));
         System.out.println(s.minSkips(new int[]{1,3,2}, 4, 2));
         System.out.println(s.minSkips(new int[]{7,3,5,5}, 2, 10));
         System.out.println(s.minSkips(new int[]{7,3,5,5}, 1, 10));
---
+++ l545/src/com/company/t3/t/Solution.java
@@ -0,0 +1,7 @@
+package com.company.t3.t;
+
+public class Solution {
+    public static void main(String[] args) {
+//        Character.isDigit()
+    }
+}
